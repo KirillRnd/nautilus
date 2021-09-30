73beb92f26cff4718310b2c35477f3ccbc2e7999
KirillRnd <kirill_rnd@mail.ru>
2021-09-30 13:34:03 +0300

    refactoring

--- Kalman_filter.m
+++ Kalman_filter.m
@@ -4,15 +4,13 @@
 %
 %
 
-clc
-clear all
-close all
+clc;
+clear;
 
-for i=1:1:20000
-    I(i)=i;
-end
+I=1:20000;
 
 M = csvread('r-gelio.csv');
+V = csvread('V-gelio.csv');
 T = csvread('t-gelio.csv');
 
 E = eye(6);
@@ -26,22 +24,27 @@ R = [1 0 0 0 0 0;
 R_1 = M(1,:) * (1 + (1e8+64e5)/norm(M(1,:))) + [0 64e5 0] ;
 R_2 = M(1,:) * (1 + (1e8+64e5)/norm(M(1,:))) + [0 -64e5 0] ;
 
-State_X = [T(2:size(T,1)),M(2:size(M,1),:)];
-for i=1:1:size(M,1)-1
-   State_X(i,5:7) = (M(i+1,:) - M(i,:))/(T(i+1)-T(i));  
-end
+State_X=[M,V];
 
-x = zeros(size(M,1),6);
-K = zeros(6,6,size(M,1));
+% State_X = [T(2:size(T,1)),M(2:size(M,1),:)];
+% for i=1:1:size(M,1)-1
+%    State_X(i,5:7) = (M(i+1,:) - M(i,:))/(T(i+1)-T(i));  
+% end
+
+x = zeros(20000,6);
+x(1,:)=ones(1,6)*1e-3;
+K = zeros(6,6,20000);
 K(:,:,1) = eye(6);
 
-for i=2:1:size(M,1)-1
-    
-    F = calc_F(State_X((i-1):i,:));
-    x_p = predict_x(F,x(i-1,:));
-    K_p = predict_K(F,K(:,:,i-1));
+for i=2:20000
+    dt = T(i)-T(i-1);
+    x_k_1 = x(i-1,:);
+    K_k_1=K(:,:,i-1);
+    F = calc_F(x_k_1,dt);
+    x_p = predict_x(F,x_k_1);
+    K_p = predict_K(F,K_k_1);
 
-    H = calc_H(State_X(i,:),R_1,R_2);
+    H = calc_H(x_k_1,R_1,R_2);
     a = calc_a(K_p,H,R);
     %y = calc_y(H);
     
@@ -54,3 +57,4 @@ for i=2:1:size(M,1)-1
     %drawnow
     
 end
+plot(T,vecnorm(x(:,1:3),2,2))
--- calc_H.m
+++ calc_H.m
@@ -1,10 +1,13 @@
 function [H] = calc_H(State,R_1,R_2)
+r=State(1:3);
+V=State(4:6);
+r_12 = r - R_1;
+r_23 = r - R_2;
 
-r_12 = State(2:4) - R_1;
-r_23 = State(2:4) - R_2;
+v_12 = r_12/norm(r_12)*dot(V,r_12/norm(r_12));
+v_23 = r_23/norm(r_23)*dot(V,r_23/norm(r_23));
 
-v_12 = r_12/norm(r_12)*dot(State(5:7),r_12/norm(r_12));
-v_23 = r_23/norm(r_23)*dot(State(5:7),r_23/norm(r_23));
+%Производная радиальной скорости
 dD = 1/2*[v_12*r_12'/norm(r_12)+v_23*r_23'/norm(r_23)];
 
 H = 1/2*[v_12'/norm(r_12)+v_23'/norm(r_23)-dD*(r_12'/norm(r_12)+r_23'/norm(r_23));
